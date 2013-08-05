0565b194ea678bf447f0bf62b9539ac39a16e415
Chris Tembreull <chris@tembreull.org>
2013-08-05 10:35:09 -0700

    Empty project with 'squeel' gem added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Org::Application.config.secret_key_base = 'b66f027b3c884c2d49ccacfa1cfcf77dcc2347fb75327924f7db6a149f0f36ed07ccc4d996d07c102fa83f968461fb033bf9804fe2259810fb8d8eb350494816'
