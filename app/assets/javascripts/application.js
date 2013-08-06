81d456ebac0ec432af9d3ab72193517f3f26bbc4
Chris Tembreull <chris@tembreull.org>
2013-08-06 16:30:45 -0700

    prettying up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap.min
+//= require select2
 //= require_tree .
--- app/models/concerns/divisions.rb
+++ app/models/concerns/divisions.rb
@@ -4,7 +4,7 @@ module Divisions extend ActiveSupport::Concern
 		scope :alphabetical, -> { order('city ASC') }
 		scope :al_west, -> { where{ abbr.like_any %w[HOU LAA OAK TEX SEA] } }
 		scope :al_central, -> { where { abbr.like_any %w[CHW CLE DET KCR MIN] } }
-		scope :al_east, -> { where{ abbr.like_any %w[NYY BOS TOR BAL TAM] } }
+		scope :al_east, -> { where{ abbr.like_any %w[NYY BOS TOR BAL TBR] } }
 		scope :nl_west, -> { where{ abbr.like_any %w[LAD SDP COL ARI SFG] } }
 		scope :nl_central, -> { where { abbr.like_any %w[CHC CIN PIT STL MIL] } }
 		scope :nl_east, -> { where{ abbr.like_any %w[PHI NYM ATL WAS MIA] } }
